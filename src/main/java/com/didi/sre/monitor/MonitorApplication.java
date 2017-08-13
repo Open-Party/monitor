feda6629f81aa539688545fc23e956b3cfd53f0a
soarpenguin <soarpenguin@gmail.com>
2017-08-13 22:37:22 +0800

    update adminlte files.

--- src/main/java/com/didi/sre/monitor/MonitorApplication.java
+++ src/main/java/com/didi/sre/monitor/MonitorApplication.java
@@ -1,13 +1,26 @@
 package com.didi.sre.monitor;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Controller
 public class MonitorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return (container -> {
+			ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401");
+			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500.html");
+
+			container.addErrorPages(error401Page, error404Page, error500Page);
+		});
 	}
 }
