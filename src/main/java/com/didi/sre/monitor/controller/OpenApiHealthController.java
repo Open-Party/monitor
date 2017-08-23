6fba49ad71a831209769df97bedc10e096dcf001
soarpenguin <soarpenguin@gmail.com>
2017-08-23 23:24:00 +0800

    update Controller to RestController.

--- src/main/java/com/didi/sre/monitor/controller/OpenApiHealthController.java
+++ src/main/java/com/didi/sre/monitor/controller/OpenApiHealthController.java
@@ -1,19 +1,17 @@
 package com.didi.sre.monitor.controller;
 
 import com.didi.sre.monitor.model.common.JsonResult;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by soarpenguin on 17-8-12.
  */
-@Controller
+@RestController
 public class OpenApiHealthController {
 
     @RequestMapping(value = "/openapi/health", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
     public JsonResult healthCheck () {
         return new JsonResult(true);
     }
