963bc0bc4eed3d48b4559a44c3954de75739d9d9
soarpenguin <soarpenguin@gmail.com>
2017-08-25 23:30:20 +0800

    add sysuser and sysrole model code.

---
+++ src/main/java/com/didi/sre/monitor/model/user/SysRoleEntity.java
@@ -0,0 +1,27 @@
+package com.didi.sre.monitor.model.user;
+
+/**
+ * Created by soarpenguin on 17-8-24.
+ */
+
+public class SysRoleEntity {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ src/main/java/com/didi/sre/monitor/model/user/SysUserEntity.java
@@ -0,0 +1,84 @@
+package com.didi.sre.monitor.model.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by soarpenguin on 17-8-24.
+ */
+public class SysUserEntity implements UserDetails {
+    private Long id;
+    private String username;
+    private String password;
+
+    private List<SysRoleEntity> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<SysRoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<SysRoleEntity> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auths = new ArrayList<>();
+        List<SysRoleEntity> roles = this.getRoles();
+        for (SysRoleEntity role : roles) {
+            auths.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return auths;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
