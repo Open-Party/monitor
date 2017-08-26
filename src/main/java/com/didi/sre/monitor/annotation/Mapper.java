b43326ba5ab8287225403399d0a7c2129f18b8f3
soarpenguin <soarpenguin@gmail.com>
2017-08-26 23:27:08 +0800

    add application properties.

---
+++ src/main/java/com/didi/sre/monitor/annotation/Mapper.java
@@ -0,0 +1,17 @@
+package com.didi.sre.monitor.annotation;
+
+import org.springframework.stereotype.Component;
+import java.lang.annotation.*;
+
+/**
+ * Created by soarpenguin on 17-8-26.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Component
+public @interface Mapper {
+    String value() default "";
+}
+
