80ae5a964dff4cc745e23dbba9b2f1e54382cfde
soarpenguin <soarpenguin@gmail.com>
2017-09-12 00:59:58 +0800

    update mysql script.

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -15,3 +15,11 @@ insert into SysRoleUser(SysUserId, SysRoleId) values(1,1);
 insert into SysRoleUser(SysUserId, SysRoleId) values(2,2);
 
 select u.*, r.name from SysUser u LEFT JOIN SysRoleUser sru on u.id=sru.SysUserId LEFT JOIN SysRole r on sru.SysRoleId=r.id where username="admin@gmail.com";
+
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+);
